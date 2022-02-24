695d22a55cbcf7a34c13edea6209289e417e1ad3
Jan Forberg <janforberg@pop-os.localdomain>
2022-02-24 09:37:02 +0100

    added databus url param

--- spotlight.sh
+++ spotlight.sh
@@ -1,38 +1,45 @@
 if [ -z ${NAME+x} ]; then NAME='en'; fi
 if [ -z ${MODEL_PATH+x} ]; then MODEL_PATH='/opt/spotlight/models'; fi
 if [ -z ${QUERY+x} ]; then QUERY='NONE'; fi
+if [ -z ${DATABUS_SPARQL_ENDPOINT+x} ]; then DATABUS_SPARQL_ENDPOINT=https://databus.dbpedia.org/repo/sparql; fi
 if [ -z ${JAVA_PARAMS+x} ]; then JAVA_PARAMS=-Xmx15G; fi
 
 echo "========================================="
 echo "NAME is set to '$NAME'"; 
 echo "MODEL_PATH is set to '$MODEL_PATH'"; 
 echo "QUERY is set to '$QUERY'"; 
+echo "QUERY is set to '$DATABUS_SPARQL_ENDPOINT'"; 
 echo "JAVA_PARAMS is set to '$JAVA_PARAMS'"; 
 echo "========================================="
 
 cd $MODEL_PATH
-
 DIRECTORY=$MODEL_PATH/$NAME
 
 if [ -d "$DIRECTORY" ]
 then
+  echo "$DIRECTORY exists. Reusing existing model from volume."
 else
-  # Create directory
-  mkdir $DIRECTORY;
   
   # Fetch the model file 
-  RESULT=`curl --data-urlencode query="$QUERY" --data-urlencode format="text/tab-separated-values" https://databus.dbpedia.org/repo/sparql | sed 's/"//g' | grep -v "^file$" | head -n 1 `
+  RESULT=`curl --data-urlencode query="$QUERY" --data-urlencode format="text/tab-separated-values" $DATABUS_SPARQL_ENDPOINT | sed 's/"//g' | grep -v "^file$" | head -n 1 `
+  
+  echo "Query Result: $RESULT"
   FILENAME=`echo "${RESULT##*/}"`
+  echo "Downloading to local file $FILENAME"
 
   # Run model download and extraction
   wget $RESULT
+
+  # Create directory
   mkdir $DIRECTORY;
-  tar -C $DIRECTORY -xvf $FILENAME
+
+  echo "Decompressing model"
+  tar -C $DIRECTORY -xf $MODEL_PATH/$FILENAME
 
   # Remove extra folder
   TMP=`ls $DIRECTORY`
-  mv $TMP/* .
-  rm -r $TMP
+  mv $DIRECTORY/$TMP/* $DIRECTORY
+  rm -r $DIRECTORY/$TMP
 
   # Remove the downloaded compressed model
   rm $FILENAME
